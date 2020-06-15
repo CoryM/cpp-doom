75142c63c4183e5ea7601a6e02da1d882069bf23
Cory Marshall <shadowwalker23901@gmail.com>
2020-06-15 14:39:15 -0400

    Fix - Angle overflow

--- src/doom/r_segs.cpp
+++ src/doom/r_segs.cpp
@@ -363,6 +363,8 @@ void R_RenderSegLoop(void)
         {
             // calculate texture offset
             angle         = (rw_centerangle + xtoviewangle[rw_x]) >> ANGLETOFINESHIFT;
+            // Angle overflows the finetangent size
+            if (angle >= std::size(finetangent)) angle = std::size(finetangent) - 1; 
             texturecolumn = rw_offset - FixedMul(finetangent[angle], rw_distance);
             texturecolumn >>= FRACBITS;
             // calculate lighting
