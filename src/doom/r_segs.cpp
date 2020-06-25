38451db27aeecf46857b92ebd13ed13b675535e4
Cory Marshall <shadowwalker23901@gmail.com>
2020-06-24 23:09:26 -0400

    Overflow/Underflow fix

--- src/doom/r_segs.cpp
+++ src/doom/r_segs.cpp
@@ -483,10 +483,10 @@ void R_RenderSegLoop(void)
 // above R_StoreWallRange
 fixed_t R_ScaleFromGlobalAngle(angle_t visangle)
 {
-    int     anglea = ANG90 + (visangle - viewangle);
-    int     angleb = ANG90 + (visangle - rw_normalangle);
-    int     den    = FixedMul(rw_distance, finesine[anglea >> ANGLETOFINESHIFT]);
-    fixed_t num    = FixedMul(projection, finesine[angleb >> ANGLETOFINESHIFT]) << detailshift;
+    angle_t angleA = ANG90 + (visangle - viewangle);
+    angle_t angleB = ANG90 + (visangle - rw_normalangle);
+    int     den    = FixedMul(rw_distance, finesine[angleA >> ANGLETOFINESHIFT]);
+    fixed_t num    = FixedMul(projection, finesine[angleB >> ANGLETOFINESHIFT]) << detailshift;
     fixed_t scale;
 
     if (den > (num >> 16))
