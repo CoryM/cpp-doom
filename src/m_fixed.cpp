bb592a07ee2efa31e28bdf156bfd322115763f18
Cory Marshall <shadowwalker23901@gmail.com>
2020-12-14 12:19:24 -0500

    shifting to floats from fixed (possible?)

--- src/m_fixed.cpp
+++ src/m_fixed.cpp
@@ -25,7 +25,15 @@
 // Fixme. __USE_C_FIXED__ or something.
 auto FixedMul(const fixed_t a, const fixed_t b) -> fixed_t
 {
+#if 0
+    constexpr auto FixedScale = static_cast<double>(1 << FRACBITS);
+    const auto     dA         = static_cast<double>(a);
+    const auto     dB         = static_cast<double>(b);
+    const auto     Product    = (dA * dB) / FixedScale;
+    return static_cast<fixed_t>(Product);
+#else
     return ((int64_t)a * (int64_t)b) >> FRACBITS;
+#endif
 }
 
 //
@@ -33,6 +41,14 @@ auto FixedMul(const fixed_t a, const fixed_t b) -> fixed_t
 //
 auto FixedDiv(const fixed_t a, const fixed_t b) -> fixed_t
 {
+#if 0
+    constexpr auto FixedScale = static_cast<double>(1 << FRACBITS);
+    const auto     dA         = static_cast<double>(a);
+    const auto     dB         = static_cast<double>(b);
+    const auto     Quotient   = (dA / dB) * FixedScale;
+
+    return static_cast<fixed_t>(Quotient);
+#else
     constexpr unsigned int MAGIC_BITSHIFT_NUMBER = 14;
     if ((abs(a) >> MAGIC_BITSHIFT_NUMBER) >= abs(b))
     {
@@ -40,4 +56,5 @@ auto FixedDiv(const fixed_t a, const fixed_t b) -> fixed_t
     }
 
     return static_cast<fixed_t>((static_cast<int64_t>(a) << FRACBITS) / b);
+#endif
 }
