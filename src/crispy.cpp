9c58d27e0aee2c97c0b836944a925d78367cf764
Cory A Marshall <shadowwalker23901@gmail.com>
2024-07-01 23:28:33 -0400

    Fix small warnings
    Missing init. warning in critical_s. some NULL to nullptr conversion, unsigned vs signed int comparision check.

--- src/crispy.cpp
+++ src/crispy.cpp
@@ -45,7 +45,7 @@ static auto crispy_s = init_crispy_s();
 crispy_t *const crispy = &crispy_s;
 
 // [crispy] "critical" config variables
-static const crispy_t critical_s = { 0 };
+static const crispy_t critical_s = { };
 const crispy_t *      critical   = &critical_s;
 
 // [crispy] update the "singleplayer" variable and the "critical" struct
